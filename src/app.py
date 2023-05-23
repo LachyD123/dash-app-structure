63e825791665bee279ea37fe78d70321e5879548
LachyD123 <38710257+LachyD123@users.noreply.github.com>
2023-05-23 14:15:30 +1000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,33 +7,13 @@ This is where we define the various css items to fetch as well as the layout of 
 # package imports
 import dash
 from dash import html
-import dash_bootstrap_components as dbc
 from flask import Flask
-from flask_login import LoginManager
-import os
 
-# local imports
-from utils.settings import APP_HOST, APP_PORT, APP_DEBUG, DEV_TOOLS_PROPS_CHECK
-from components.login import User, login_location
-from components import navbar, footer
 
 server = Flask(__name__)
 app = dash.Dash(
     __name__,
-    server=server,
-    use_pages=True,    # turn on Dash pages
-    external_stylesheets=[
-        dbc.themes.BOOTSTRAP,
-        dbc.icons.FONT_AWESOME
-    ],  # fetch the proper css items we want
-    meta_tags=[
-        {   # check if device is a mobile device. This is a must if you do any mobile styling
-            'name': 'viewport',
-            'content': 'width=device-width, initial-scale=1'
-        }
-    ],
-    suppress_callback_exceptions=True,
-    title='Dash app structure'
+    server=server
 )
 
 
@@ -44,10 +24,7 @@ server = app.server         # the server is needed to deploy the application
 
 if __name__ == "__main__":
     app.run_server(
-        host=APP_HOST,
-        port=APP_PORT,
-        debug=APP_DEBUG,
-        dev_tools_props_check=DEV_TOOLS_PROPS_CHECK
+      
     )
 
 
